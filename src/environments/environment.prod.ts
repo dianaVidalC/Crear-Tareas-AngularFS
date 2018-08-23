5526da5e136f019633cd7e5f753d894771eaa3c9
Erika Vidal <diann852@gmail.com>
2018-08-23 00:44:51 -0500

    deploy gh-pages 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,12 @@
 
-import { environment as common } from './environment';
-
 export const environment = {
-  ...common,
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBaz0hHF-mRW_hspqysBSNGWnHEi8U-CcQ",
+    authDomain: "task-angular-8432a.firebaseapp.com",
+    databaseURL: "https://task-angular-8432a.firebaseio.com",
+    projectId: "task-angular-8432a",
+    storageBucket: "task-angular-8432a.appspot.com",
+    messagingSenderId: "469304228635"
+  }
 };
